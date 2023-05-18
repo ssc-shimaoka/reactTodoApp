956f06401d2a79a1fdd2078c68eb9ce31985b986
ssc_shimaoka <yuki_shimaoka@solver.co.jp>
2023-05-18 11:01:10 +0900

    コメント追加

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 export const Todo = ({todo, toggleTodo}) => {
+
+  /**
+   * チェックボックス押下時のイベント
+   * 押したタスクのidを引数に指定し、App.jsのtoggleTodo()を呼ぶ
+   */
   const handleTodoClick = () => {
     toggleTodo(todo.id);
   };
 
+  // 表示出力
   return (
     <div>
         <label>
@@ -21,4 +27,5 @@ export const Todo = ({todo, toggleTodo}) => {
 
 };
 
+// 出力
 export default Todo;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Todo from './Todo';
 
+// Todoリストコーポネント出力
 const TodoList = ({todos, toggleTodo}) => {
   return todos.map((todo) => <Todo todo={todo} key={todo.id} toggleTodo={toggleTodo}/>);
 };
 
+// 出力
 export default TodoList;
