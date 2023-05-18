ab1137cc90cf90b84f380b3a86da1dfe28cc9f05
ssc_shimaoka <yuki_shimaoka@solver.co.jp>
2023-05-18 10:37:20 +0900

    タスク数管理・タスク削除実装・不具合修正

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import TodoList from './TodoList';
 import {v4 as uuidv4} from "uuid";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {id: 1, name: "Todo1", completed: false},
-]);
-
+  const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
 
   const handleAddTodo = () => {
     //タスクを追加
     //console.log(todoNameRef.current.value);
     const name = todoNameRef.current.value;
+    if(name === "") return;
     setTodos((prevTodos) => {
       return [...prevTodos, {id: uuidv4(), name: name, completed: false}];
     });
@@ -27,13 +25,18 @@ function App() {
     setTodos(newTodos);
   };
 
+  const handleClrar = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
   return (
     <>
-      <TodoList todos={todos}/>
-      <input type="text" ref={todoNameRef} toggleTodo={toggleTodo}/>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input type="text" ref={todoNameRef} />
       <button onClick={handleAddTodo}>タスクを追加</button>
-      <button>完了したタスクの削除</button>
-      <div>残りのタスク：0</div>
+      <button onClick={handleClrar}>完了したタスクの削除</button>
+      <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
     </>
   );
 }
